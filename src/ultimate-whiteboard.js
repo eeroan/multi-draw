b75355cf9f8b773abc86dbf0eefdf399476cce5f
Eero Anttila <eea@iki.fi>
2011-01-30 15:32:09 +0200

    refactor

--- src/ultimate-whiteboard.js
+++ src/ultimate-whiteboard.js
@@ -3,40 +3,14 @@ var players = $('.player')
 var canvasDom = canvas.get(0)
 var ctx = $.extend(canvasDom.getContext("2d"), {strokeStyle: "rgba(0, 0, 200, 1.0)", lineWidth: 5,lineCap: "round"})
 
-var mouseDown = canvas.toObservable('mousedown')
-var touchStart = canvas.toObservable('touchstart').Where(notPinch)
-
-var pencilDown = mouseDown.Merge(touchStart)
+var pencilDown = startOn(canvas)
 var mouseMove = $(document).toObservable('mousemove')
 var touchMove = $(document).toObservable('touchmove').Where(notPinch)
+mouseMove.Merge(touchMove).Subscribe(preventDefault)
 
-var move = mouseMove.Merge(touchMove)
-move.Subscribe(preventDefault)
-var mouseUp = $(document).toObservable('mouseup touchend')
-var playerMoveStart = $('.player').toObservable('mousedown touchstart')
-var pencilMoves = move.Select(getEvent).Select(point).SkipUntil(pencilDown).TakeUntil(mouseUp)
-var curve = pencilMoves.Zip(pencilMoves.Skip(1), function(prev, cur) {
-  return [prev, cur, ctx]
-})
-
-curve.Repeat().Subscribe(function(line) {
-  drawLine.apply(null, line)
-})
-
-
-var playerMoves = move.Select(getEvent).Select(point).SkipUntil(playerMoveStart).TakeUntil(mouseUp)
-var playerPos = playerMoves.Zip(playerMoves.Skip(1), function(prev, cur) {
-  //TODO cur.target voi osua divin ulkopuolelle. pitää olla combine latest tai vastaava
-  //TODO pelaajan siirto ei toimi ipadilla
-  return [prev,cur]
-})
-
-var combined = playerMoveStart.CombineLatest(playerPos.Repeat(), function(start,pos) {
-  return [start.target, pos]
-})
-combined.Subscribe(function(pos) {
-  movePlayer($(pos[0]),pos[1][0],pos[1][1])
-})
+var playerMoveStart = startOn(players).Select(targetElement)
+movesAfter(pencilDown).Repeat().Subscribe(drawLine)
+playerMoveStart.CombineLatest(movesAfter(playerMoveStart).Repeat(), argumentList).Subscribe(movePlayer)
 
 var clear = $('#clear').toObservable('click')
 clear.Subscribe(function() {
@@ -45,7 +19,48 @@ clear.Subscribe(function() {
   ctx.closePath()
 })
 
-function movePlayer(player,oldMouse, newMouse) {
+function movesAfter(startEvent) {
+  var mouseUp = $(document).toObservable('mouseup touchend')
+  var move = mouseMove.Merge(touchMove.Select(touchEvent)).Select(point)
+  return delta(move.SkipUntil(startEvent).TakeUntil(mouseUp))
+}
+function delta(moves) {
+  return moves.Zip(moves.Skip(1), argumentList)
+
+}
+function startOn(container) {
+  var mouseDown = container.toObservable('mousedown')
+  var touchStart = container.toObservable('touchstart').Where(notPinch)
+  return mouseDown.Merge(touchStart)
+
+}
+function notPinch(evt) {
+  return evt.originalEvent.touches.length == 1
+}
+function touchEvent(evt) {
+  return evt.originalEvent.touches[0]
+}
+function preventDefault(e) {
+  e.preventDefault()
+}
+
+function targetElement(e) {
+  return $(e.target)
+}
+
+function drawLine(line) {
+  ctx.beginPath()
+  ctx.moveTo.apply(ctx, line[0])
+  ctx.lineTo.apply(ctx, line[1])
+  ctx.stroke()
+  ctx.closePath()
+}
+
+function movePlayer(playerAndDelta) {
+  var player = playerAndDelta[0]
+  var delta = playerAndDelta[1]
+  var oldMouse = delta[0]
+  var newMouse = delta[1]
   var deltaX = newMouse[0] - oldMouse[0]
   var deltaY = newMouse[1] - oldMouse[1]
   player.moveRelatively([deltaX,deltaY])
@@ -60,28 +75,8 @@ function point(e) {
    */
 }
 
-function notPinch(evt) {
-  return evt.originalEvent.touches.length == 1
-}
-function ipadEvent(evt) {
-  return evt.originalEvent.touches
-}
-function getEvent(evt) {
-  if (ipadEvent(evt)) {
-    return evt.originalEvent.touches[0]
-  } else {
-    return evt
-  }
-}
-function preventDefault(e) {
-  e.preventDefault()
-}
-function drawLine(start, end, ctx) {
-  ctx.beginPath()
-  ctx.moveTo.apply(ctx, start)
-  ctx.lineTo.apply(ctx, end)
-  ctx.stroke()
-  ctx.closePath()
+function argumentList() {
+  return arguments
 }
 $.fn.moveRelatively = function(pos) {
   var deltaX = pos[0]
@@ -92,5 +87,6 @@ $.fn.moveRelatively = function(pos) {
   this.css(css)
   return this
 }
-players.each(function(i) {$(this).moveRelatively([50*i,100])})
-
+players.each(function(i) {
+  $(this).moveRelatively([50 * i,100])
+})
