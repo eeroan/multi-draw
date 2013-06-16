4f24522c97531017cf7315fe80bba22507ca2da4
Eero Anttila <eea@iki.fi>
2013-06-16 23:15:43 +0300

    Upload drawings with password and filename

--- gallery.js
+++ gallery.js
@@ -8,14 +8,32 @@ $('#gallery').html($.map(savedMultiDrawImages,function (id) {
     '</div>'
 }).join(''))
 $('.save').click(function () {
-  var id = $(this).attr('href').substring(1)
+  var id = hash(this)
   var dataUrl = localStorage.getItem(id)
-  $.post("http://eea.kapsi.fi/dataUrl.php", { img: dataUrl.substring(dataUrl.indexOf(',')+1)} );
+  dataUrl = dataUrl.substring(dataUrl.indexOf(',') + 1)
+  var password = localStorage.getItem('img-pwd')
+  post(password || promptPwd())
+  function post(password) {
+    return $.post("http://eea.kapsi.fi/dataUrl.php", {
+      img     : dataUrl,
+      password: password,
+      id      : id
+    })//.fail(function () { post(promptPwd()) })
+  }
+
+  function promptPwd() {
+    var password = prompt('Enter the password for image server')
+    localStorage.setItem('img-pwd', password)
+    return password
+  }
 })
+
 $('.remove').click(function () {
-  var id = $(this).attr('href').substring(1)
+  var id = hash(this)
   localStorage.removeItem(id)
   savedMultiDrawImages.splice(savedMultiDrawImages.indexOf(id), 1)
   localStorage.setItem(key, JSON.stringify(savedMultiDrawImages))
   $(this).parents('.image').remove()
 })
+
+function hash(elem) {return $(elem).attr('href').substring(1) }
