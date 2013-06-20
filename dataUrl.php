df850198c745f6ae8091c811d6e6b1f9afa30779
Eero Anttila <eea@iki.fi>
2013-06-20 16:21:33 +0300

    Put json data to response

--- dataUrl.php
+++ dataUrl.php
@@ -4,11 +4,17 @@ $pwd = $_POST["password"];
 $id = $_POST["id"];
 $settings = parse_ini_file("./pwd.ini");
 $realPwd = $settings['password'];
-if($pwd != $realPwd) throw new Exception('Invalid pwd:'.$pwd.':'.$realPwd.'lol');
+if($pwd != $realPwd) throw new Exception('Invalid pwd:' . $pwd . ':' . $realPwd . 'lol');
 $image = base64_decode($img);
 echo "OK";
-$myFile = "upload/".$id.".jpg";
+$myFile = "upload/" . $id . ".jpg";
 $fh = fopen($myFile, 'w');
 fwrite($fh, $image);
 fclose($fh);
+header('Content-Type: application/json; charset=utf-8', 200);
+$arr = array(
+    'stack'=>'overflow',
+    'key'=>'value'
+);
+echo json_encode($arr);
 ?>
