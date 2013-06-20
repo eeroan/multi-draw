faa0a78d72ef396dbac0475158e3469b36031bc5
Eero Anttila <eea@iki.fi>
2013-06-20 17:00:00 +0300

    Set response codes properly #2

--- dataUrl.php
+++ dataUrl.php
@@ -12,12 +12,14 @@ try {
     $fh = fopen($myFile, 'w');
     fwrite($fh, $image);
     fclose($fh);
-    header('Content-Type: application/json; charset=utf-8', $code);
+    header('Content-Type: application/json; charset=utf-8');
+    http_response_code($code);
     $arr = array('message' => $code);
     echo json_encode($arr);
 } catch(Exception $e) {
     $error = array('message' => $e->getMessage());
-    header('Content-Type: application/json; charset=utf-8', 500);
+    header('Content-Type: application/json; charset=utf-8');
+    http_response_code(500);
     echo json_encode($arr);
 }
 ?>
