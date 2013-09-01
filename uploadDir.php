f2558818e3874b459849d80bb01d5dffe5e1cc1c
Eero Anttila <eea@iki.fi>
2013-09-01 15:12:36 +0300

    add missing header call to uploadDir.php

--- uploadDir.php
+++ uploadDir.php
@@ -1,5 +1,6 @@
 <?php
 $code = 200;
 $contentType = 'Content-Type: application/json; charset=utf-8';
+header($contentType, true, $code);
 echo json_encode(glob("upload/*.{jpg,gif,png}", GLOB_BRACE));
 ?>
